49e3744535e253a7af107b2b2b9ec79576be7a96
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-12 08:19:47 -0700

    macro

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int a;
 	long int b;
@@ -11,7 +15,7 @@ int main (void)
 	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
