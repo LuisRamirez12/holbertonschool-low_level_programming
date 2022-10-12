46291b3906f1d220b6ae471d9ec1f72213e9f35e
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-12 10:08:06 -0700

    cielo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,5 +13,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 98)
 printf("%d is positive\n", n);
+else if (n < -98)
+printf("%d Last digit of -98 is -8 and is less than 6 and not 0\n", n);
 return (0);
 }
