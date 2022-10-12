d7ddd33b6180697312002e8ce9e26bf85ccce521
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-12 10:02:34 -0700

    yeah

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,11 +13,5 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
-if (n = 98)
-printf("%d is zero\n", n);
-if (n = -98)
-printf("%d is zero\n", n);
-if (n = 980)
-printf("%d is zero\n", n);
 return (0);
 }
