09b09bb23f57033d779404ed4f1c4e838ae622c1
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-21 14:12:33 -0700

    tapa

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,6 +14,6 @@ n = rand() - RAND_MAX / 2;
 if (n % 10 > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 else if (n % 34 > 0)
-printf("Last digit of %d n is %d 0 the string and is 0\n", n, n % 34);
+printf("Last digit of %d n is 0 the string and is 0\n", n, n % 34);
 return (0);
 }
