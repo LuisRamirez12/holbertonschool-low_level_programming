a47b581775d1c66d75ca1cf02e6a0ea702843589
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-21 14:27:40 -0700

    pared

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,6 +15,7 @@ if (n % 10 > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 else if (n % 987 > 0)
 printf("Last digit of %d is 0 and is 0\n", n % 987);
-
+else if (n % -98 -8 > 0)
+	printf("Last digit of %d -98 is -8 and is less than 6 and not 0\n", n % -98);
 return (0);
 }
