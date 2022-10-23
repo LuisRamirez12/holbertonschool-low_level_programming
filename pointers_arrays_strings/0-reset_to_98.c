84279a5afd4dcd4d84c54d4644d7d9ad31279203
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-23 12:55:57 -0700

    hola

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -9,3 +9,16 @@ void reset_to_98(int *n)
 {
 	*n= 98;
 }
+
+/**
+ * main - parameters are passeb by value
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	int n;
+	n = 98;
+	reset_to_98(*n);
+	return (0);
+}
