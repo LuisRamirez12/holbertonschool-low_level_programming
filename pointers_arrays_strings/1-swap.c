b66649fd88551ac3e26edecb0803f42bf8898239
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-23 14:34:48 -0700

    mundo

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -5,10 +5,11 @@
  *
  * Return: Always 0.
  */
-void swap_int(int *a, int *b);
-(
-int a;
-int b;
-a = 98;
-b = 42;
-)
+void swap_int(int *a, int *b)
+{
+	int a_original = *a;
+	int b_original = *b;
+
+	*a = b_original;
+	*b = a_original;
+}
