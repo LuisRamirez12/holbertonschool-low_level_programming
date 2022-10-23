08ed61f36b9712d08f3dc91684e89945a8f58268
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-23 16:15:44 -0700

    teclado

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,13 @@
  * Return: Always 0.
  */
 int _strlen(char *s);
+int main(void)
 {
-int length = 0;
-while (*s != '\0')	
-{
-s++
-length++;
-}
-return (length);
+		char *str;
+			int len;
+
+				str = "Holberton!";
+					len = _strlen(str);
+						printf("%d\n", len);
+							return (0);
 }
