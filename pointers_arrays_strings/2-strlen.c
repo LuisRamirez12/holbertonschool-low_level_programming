1d935eb866ecdbaea7ffa0b1c88496f1da89156d
LuisRamirez12 <rookhooperr24@gmail.com>
2022-10-23 15:58:50 -0700

    lupa

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <strlen>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s);
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+}
